40db25c7b699cd98da68375f0610490b0c24fb47
piyushbaibhav <piyushbaibhav12@gmail.com>
2024-03-29 11:29:44 +0530

    link button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="flex justify-between w-full p-4 h-4 bg-black text-white">
   <h1 className=" text-4xl font-medium underline underline-offset-[10px]"style={{ color: 'rgb(206 255 252)' }}>Pomodoro </h1>
-  <div className="flex justify-center pr-11 pb-11 mt-8" style={{ color: 'rgb(255 173 212)' }}>
+  <div className="flex justify-center pr-11 pb-11 z-10 mt-8" style={{ color: 'rgb(255 173 212)' }}>
       <a className="mr-4" href="https://github.com/piyushbaibhav" target="_blank" rel="noopener noreferrer">
         GitHub
       </a>
